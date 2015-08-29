07fea634d5d2ef0d4934ecfc8abe0fc07a4fc103
XGHeaven <619943612@qq.com>
2015-08-29 14:13:08 +0800

    server push

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ var server = http2.createServer({
 	key: fs.readFileSync('privatekey.pem'),
 	cert: fs.readFileSync('certificate.pem')
 }, function(req, res) {
-	res.end('hello world');
+	var push = res.push('/client.js')
+	push.end('alert("this is a server push");');
+	fs.createReadStream('index.html').pipe(res);
 });
 
 server.listen(443, function() {
